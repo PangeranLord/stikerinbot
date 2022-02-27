a66cebabab82450058f2f97ad65298d17a5b6b70
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 21:13:36 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -10,7 +10,7 @@ handler.before = async (m) => {
         let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'id' }))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        if (json.success == "Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.") return m.reply('simi nya gk tau bang')
+        if (json.success == "Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.") return m.reply('lu ngomong apa')
         m.reply(json.success)
         return !0
     }

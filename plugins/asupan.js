60fb8a9a06f3d69340f752b8631fdd89d750457e
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 20:08:00 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,8 +1,10 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
+  conn.sendFile(m.chat, API('amel', '/asupan', {}, 'apikey'), 'asupan.mp4', wm)
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
 handler.command = /^(asupan)$/i
+handler.limit = true
+handler.premium = true
 
 module.exports = handler
